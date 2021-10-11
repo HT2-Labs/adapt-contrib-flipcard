d2e355b6f1bdeddba24e028a6f696476d52e3747
ethan-lp <ethan@learningpool.com>
2021-10-11 11:09:04 +0100

    reorganisation of classes and small less change

--- templates/flipcard.jsx
+++ templates/flipcard.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 import { templates, classes, html, compile } from 'core/js/reactHelpers';
 
 export default function flipcard(props) {
-  const ariaLabels = Adapt.course.get('_globals')._accessibility._ariaLabels;
-
   const {
-    _flipType,
-    _flipTime,
     displayTitle,
     body,
     instruction
@@ -15,55 +11,54 @@ export default function flipcard(props) {
 
   return (
     <div
-      className='flipcard__inner component__inner' 
+      className='component__inner flipcard__inner' 
       role='region'
-      aria-label='_globals._components._flipcard.ariaRegion'
     >
 
       <templates.header {...props} />
 
-      <div className='flipcard__widget component__widget clearfix'>
+      <div className='component__widget flipcard__widget clearfix'>
 
         {props._items.map(({ _index, backBody, backTitle, frontImage, _flipDirection }, index) =>
           
           <div 
             className={classes([
-              `flipcard__item component__item item-${index} ${_flipDirection}`,
+              `component__item flipcard__item item-${index} ${_flipDirection}`,
             ])}
             aria-labelledby={(displayTitle || body || instruction)}
             key={_index}
           >
-            <div className='flipcard__item-face flipcard__item-front'>
+            <div
+              id={_index}
+              className='flipcard__item-face flipcard__item-front'>
               <img
                 className='flipcard__item-frontImage'
-                src={frontImage.src} 
+                src={frontImage.src}
                 aria-label={frontImage.alt}>
               </img>
             </div>
 
-          <div className='flipcard__item-face flipcard__item-back'>
+            <div className='flipcard__item-face flipcard__item-back'>
+              {backTitle &&
+                <div 
+                  className='flipcard__item-back-title'
+                  aria-live='polite'
+                >
+                {html(compile(backTitle))}
+                </div>
+              }
 
-            {backTitle &&
-              <div 
-                className='flipcard__item-back-title'
-                aria-live='polite'
-              >
-              {html(compile(backTitle))}
-              </div>
-            }
-
-            {backBody &&
-              <div 
-                className='flipcard__item-back-body'
-                aria-live='polite'
-              >
-              {html(compile(backBody))}
-              </div>
-            }
-
+              {backBody &&
+                <div 
+                  className='flipcard__item-back-body'
+                  aria-live='polite'
+                >
+                {html(compile(backBody))}
+                </div>
+              }
+            </div>
+            
           </div>
-          </div>
-
         )}
       </div>
 
